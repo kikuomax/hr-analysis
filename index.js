5b40cd9501c884c4a46b320ea1a4df569a1fd8c6
Kikuo Emoto <kikuomax@gmail.com>
2017-06-22 00:43:15 -0400

    hr-analysis: tokens are cached
     - The tokens are cached when a signin succeeds. Tokens are stored in
       a single global cache, though, it is OK because this is a personal
       application.

--- index.js
+++ index.js
@@ -9,85 +9,113 @@ server.connection({
 	host: 'localhost'
 });
 
-// configures the authentication module 'bell'
-server.register(require('bell'), (err) => {
-	if (err) {
-		throw err;
-	}
-	server.auth.strategy('fitbit', 'bell', {
-		provider: 'fitbit',
-		password: process.env.COOKIE_PASSWORD,
-		clientId: process.env.FITBIT_OAUTH2_CLIENT_ID,
-		clientSecret: process.env.FITBIT_OAUTH2_CLIENT_SECRET,
-		isSecure: false,
-		scope: ['profile', 'activity', 'heartrate', 'sleep']
-	});
-	// makes sure that the 'fitbit' strategy is registered before
-	// the signin route is added
-	server.route({
-		method: 'GET',
-		path: '/signin',
-		config: {
-			auth: 'fitbit',
-			handler: (request, reply) => {
-				if (!request.auth.isAuthenticated) {
-					return reply('Authentication failed due to: ');
-				}
-				const requestOptions = {
-					headers: {
-						Authorization:
-							`Bearer ${ request.auth.credentials.token }`
-					},
-					json: true
-				};
-				Wreck.get(
-					'https://api.fitbit.com/1/user/-/profile.json',
-					requestOptions,
-					(err, response, payload) => {
-						if (err) {
-							console.log(err);
-							return reply(err);
-						}
-						return reply(`<pre>${ JSON.stringify(payload) }</pre>`);
-					});
+// single session is sufficient because this is a personal application
+const cache = server.cache({
+	segment: 'session',
+	expiresIn: 24 * 60 * 60 * 1000
+});
+
+server.register([
+	require('bell'),
+	{
+		register: require('good'),
+		options: {
+		reporters: {
+				console: [{
+					module: 'good-squeeze',
+					name: 'Squeeze',
+					args: [{
+						response: '*',
+						log: '*'
+					}]
+				}, {
+					module: 'good-console'
+				}, 'stdout']
 			}
 		}
-	});
-});
-
-server.route({
-	method: 'GET',
-	path: '/',
-	handler: function (request, reply) {
-		reply(`Hello, world!`);
-	}
-});
-
-server.register({
-	register: require('good'),
-	options: {
-		reporters: {
-			console: [{
-				module: 'good-squeeze',
-				name: 'Squeeze',
-				args: [{
-					response: '*',
-					log: '*'
-				}]
-			}, {
-				module: 'good-console'
-			}, 'stdout']
-		}
-	}
-}, (err) => {
-	if (err) {
-		throw err;
-	}
-	server.start((err) => {
+	}],
+	(err) => {
 		if (err) {
 			throw err;
 		}
-		console.log(`Server running at: ${server.info.uri}`);
+		server.auth.strategy('fitbit', 'bell', {
+			provider: 'fitbit',
+			password: process.env.COOKIE_PASSWORD,
+			clientId: process.env.FITBIT_OAUTH2_CLIENT_ID,
+			clientSecret: process.env.FITBIT_OAUTH2_CLIENT_SECRET,
+			isSecure: false,
+				// should be true, but the localhost version needs false
+			scope: ['profile', 'activity', 'heartrate', 'sleep']
+		});
+		server.route({
+			method: 'GET',
+			path: '/',
+			handler: (request, reply) => {
+				reply(`Hello, world!`);
+			}
+		});
+		server.route({
+			method: 'GET',
+			path: '/signin',
+			config: {
+				auth: 'fitbit',
+				handler: (request, reply) => {
+					if (!request.auth.isAuthenticated) {
+						return reply('Authentication failed due to: ');
+					}
+					cache.set(
+						'tokens',
+						{
+							token: request.auth.credentials.token,
+							refreshToken: request.auth.credentials.refreshToken
+						},
+						0,
+						(err) => {
+							if (err) {
+								console.log(err);
+								return reply(err);
+							}
+						});
+					return reply(
+						'Logged in as ' +
+						request.auth.credentials.profile.displayName);
+				}
+			}
+		});
+		server.route({
+			method: 'GET',
+			path: '/profile',
+			handler: (request, reply) => {
+				cache.get('tokens', (err, cached) => {
+					if (err) {
+						console.log(err);
+						return reply(err);
+					}
+					const requestOptions = {
+						headers: {
+							Authorization: `Bearer ${ cached.token }`
+						},
+						json: true
+					};
+					Wreck.get(
+						'https://api.fitbit.com/1/user/-/profile.json',
+						requestOptions,
+						(err, response, payload) => {
+							if (err) {
+								console.log(err);
+								return reply(err);
+							}
+							return reply(
+								`<pre>${ JSON.stringify(payload) }</pre>`);
+						});
+				});
+			}
+		});
+		server.start((err) => {
+			if (err) {
+				throw err;
+			}
+			console.log(`Server running at: ${server.info.uri}`);
+		});
 	});
-});
 
